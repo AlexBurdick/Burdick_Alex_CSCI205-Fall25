d0408623d1a54cf6ba883fb8e441c707eb67a201
AlexBurdick <alexanderw.burdick@gmail.com>
2025-10-18 19:02:25 -0400

    commit

--- Week_8/open_address_main.cpp
+++ Week_8/open_address_main.cpp
@@ -8,8 +8,8 @@
 using namespace std;
 
 int main() {
-    int cap = 11;
-    string str = "test";
+    int cap = 5;
+    string arr[5] = {"string1", "string2", "string3", "string4", "string5"};
 
     OpenHashTable<int> ht(cap);
     for (int i = 0; i < cap; i++) {
